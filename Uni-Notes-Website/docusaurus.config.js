2ff9e4d7ac2d63ca22c0c1ed9ade1cb6a84ded16
Nathan Burgess <nathan.burgessdev@gmail.com>
2023-01-31 14:12:56 +0000

    vault backup: 2023-01-31 14:12:56,,1

--- Uni-Notes-Website/docusaurus.config.js
+++ Uni-Notes-Website/docusaurus.config.js
@@ -14,12 +14,13 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Uni-Notes-Website',
+  baseUrl: '/Uni-Notes-Website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'NathanBurgessDev', // Usually your GitHub org/user name.
+  projectName: 'Uni-Notes-Website', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
